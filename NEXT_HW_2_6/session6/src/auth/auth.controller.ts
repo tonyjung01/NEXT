03af5c5fe49eb7f34c522e19af98662a5602130b
정재윤 <jaeyoonjung01@gmail.com>
2024-11-14 15:53:43 +0900

    nestjs life cycle

---
+++ NEXT_HW_2_6/session6/src/auth/auth.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Post, Body, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+    const user = await this.authService.validateUser(
+      loginDto.username,
+      loginDto.password,
+    );
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+    setTimeout(() => {
+      console.log('로그인 되었습니다!');
+    }, 10000);
+    return this.authService.login(user);
+  }
+}
